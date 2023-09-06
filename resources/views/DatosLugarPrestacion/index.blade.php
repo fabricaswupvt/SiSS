dda206b4053d0206147ddb05a2bb4956d58015ef
Alix Lopez <alixnava2@gmail.com>
2023-09-06 12:35:02 -0600

    Modificacion de la vista lugar de prestacion y se agrego controlador

---
+++ resources/views/DatosLugarPrestacion/index.blade.php
@@ -0,0 +1,154 @@
+@extends('layout.template')
+
+@section('title', 'Datos Lugar de Prestacion')
+
+@section('content')
+<div class="row justify-content-center">
+  <div class="col-md-8">
+  <h2 class="text-center mb-5">Datos de lugar de prestación</h2>
+    <form class="row g-3">
+
+      {{--Informacion de lugar de prestracion--}}
+      <label class="form-label mb-1" style="font-weight: bold;">Información de lugar de prestación</label>
+          <div class="row">
+            <div class="col-md-4">
+              <label class="form-label">RFC</label>
+              <input type="text" class="form-control" name="rfc" id="rfc" value="{{old('rfc')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Nombre de lugar de prestación</label>
+              <input type="text" class="form-control" name="nombre_lp" id="nombre_lp" value="{{old('nombre_lp')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label for="sector" class="form-label">Sector</label>
+              <select class="form-select" aria-label="Default select example" name="sector" id="sector">
+                <option selected>Selecciona una opción</option>
+                <option value="publico" {{ old('sector') == 'publico' ? 'selected' : '' }}>Público</option>
+                <option value="privado" {{ old('sector') == 'privado' ? 'selected' : '' }}>Privado</option>
+              </select>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Tipo</label>
+              <select class="form-select" aria-label="Default select example" name="tipolp" id="tipolp">
+                <option selected>Selecciona una opción</option>
+                <option value="federal" {{ old('tipolp') == 'federal' ? 'selected' : '' }}>Federal</option>
+                <option value="estatal" {{ old('tipolp') == 'estatal' ? 'selected' : '' }}>Estatal</option>
+                <option value="municipal" {{ old('tipolp') == 'municipal' ? 'selected' : '' }}>Municipal</option>
+              </select>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Giro</label>
+              <input type="text" class="form-control" name="giro" id="giro" value="{{old('giro')}}" required>
+            </div>
+          </div>
+          {{--Informacion de Dirección--}}
+          <label class="form-label mb-1" style="font-weight: bold;">Información de Dirección:</label>
+          <div class="row">
+            <div class="col-md-4">
+              <label class="form-label">Calle</label>
+              <input type="text" class="form-control" name="calle" id="calle" value="{{old('calle')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Nº Exterior</label>
+              <input type="text" class="form-control" name="no_ext" id="no_ext" value="{{old('no_ext')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Nº Interior</label>
+              <input type="text" class="form-control" name="no_int" id="no_int" value="{{old('no_int')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Referencia para llegar</label>
+              <input type="text" class="form-control" name="referencia" id="referencia" value="{{old('referencia')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Colonia</label>
+              <input type="text" class="form-control" name="nombre_col" id="nombre_col" value="{{old('nombre_col')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Código postal</label>
+              <input type="text" class="form-control" name="cp" id="cp" value="{{old('cp')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Ciudad</label>
+              <input type="text" class="form-control" name="nombre_ciudad" id="nombre_ciudad" value="{{old('nombre_ciudad')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">País</label>
+              <select class="form-select" name="nombre_pais" id="nombre_pais" aria-label="Default select example">
+                  <option selected>Selecciona una opción</option>
+                  @foreach($paises as $pais)
+                      <option value="{{ $pais }}">{{ $pais }}</option>
+                  @endforeach
+              </select>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Estado</label>
+              <input type="text" class="form-control" name="nombre_edo" id="nombre_edo" value="{{old('nombre_edo')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Municipio</label>
+              <input type="text" class="form-control" name="nombre_num" id="nombre_num" value="{{old('nombre_num')}}" required>
+            </div>
+          </div>
+          {{--Informacion de Responsable del lugar de prestación--}}
+          <label class="form-label mb-1" style="font-weight: bold;">Información del responsable del lugar de prestación:</label>
+          <div class="row">
+          <div class="col-md-4">
+              <label class="form-label">Nombre del departamento</label>
+              <input type="text" class="form-control" name="nombre_depto" id="nombre_depto" value="{{old('nombre_depto')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Nombre del responsable</label>
+              <input type="text" class="form-control" name="nombre" id="nombre" value="{{old('nombre')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Apellido paterno</label>
+              <input type="text" class="form-control" name="ape_pat" id="ape_pat" value="{{old('ape_pat')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Apellido materno</label>
+              <input type="text" class="form-control" name="ape_mat" id="ape_mat" value="{{old('ape_mat')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Título</label>
+              <input type="text" class="form-control" name="titulo" id="titulo" value="{{old('titulo')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Cargo</label>
+              <input type="text" class="form-control" name="cargo_responsable" id="cargo_responsable" value="{{old('cargo_responsable')}}" required>
+            </div>
+          </div>
+
+          {{--Informacion del responsable--}}
+        <label class="form-label mb-1" style="font-weight: bold;">Contacto con responsable:</label>
+        <div class="row">
+            <div class="col-md-4">
+              <label class="form-label">Teléfono fijo</label>
+              <input type="text" class="form-control" name="telefono_fijo" id="telefono_fijo" value="{{old('telefono_fijo')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Teléfono celular</label>
+              <input type="text" class="form-control" name="celular" id="celular" value="{{old('celular')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label class="form-label">Teléfono de referencia</label>
+              <input type="text" class="form-control" name="telefono_ref" id="telefono_ref" value="{{old('telefono_ref')}}" required>
+            </div>
+            <div class="col-md-4">
+              <label for="exampleFormControlInput1" class="form-label">Correo</label>
+              <input type="text" class="form-control" name="correo" id="correo" value="{{old('correo')}}" required>
+            </div>
+        </div> 
+        <div class="d-flex justify-content-center">
+          <button type="submit" class="btn btn-success col-md-2 mx-2">Guardar</button>
+          <a href="{{url('menu')}}" class="btn btn-success col-md-2 mx-2">Cancelar</a>
+        </div>
+        <div>
+          
+        </div>
+      </form>
+
+
+  </div>
+</div>
+@endsection
--- resources/views/Menu/index.blade.php
+++ resources/views/Menu/index.blade.php
@@ -4,6 +4,84 @@
 
 @section('content')
 
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Menú</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Proyectos ofertados
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Registrar Proyecto</a></li>
+            <li><a class="dropdown-item" href="#">Ver Proyecto</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Coordinadores
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Registrar Coordinador</a></li>
+            <li><a class="dropdown-item" href="#">Ver Coordinador</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Áreas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Registrar Área</a></li>
+            <li><a class="dropdown-item" href="#">Ver Áreas</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Plantel
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Registrar Plantel</a></li>
+            <li><a class="dropdown-item" href="#">Consultar Plantel</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Lugar de Prestación
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Registrar Lugar de Prestación</a></li>
+            <li><a class="dropdown-item" href="#">Consultar Lugar de Prestación</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Usuario
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Registrar Usuario</a></li>
+            <li><a class="dropdown-item" href="#">Consultar Usuario</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Estadísticas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-right-from-bracket"></i>Salir</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-light" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+    </div>
+  </div>
+</nav>
+
 <main>
     <div class="container py-4">
         <h2>Menú de direccionamiento</h2>
